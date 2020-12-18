5995a19dfdf0b4ed724e49bb4d42d7e57a8b75e7
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-12-17 19:11:35 -0600

    initial setup for servers and mongodb

---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Mindful: require('./mindful')
+};
---
+++ server/models/mindful.js
@@ -0,0 +1,16 @@
+mongoose = require('mongoose');
+
+const MindfulSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Mindful = mongoose.model('Mindful', MindfulSchema);
+
+module.exports = Mindful;
