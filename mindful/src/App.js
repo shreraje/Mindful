256739a6452dad618840c6e3e7f2dd68f7c8a17b
Rajendra Shrestha <rajendrashr@gmail.com>
2020-12-21 21:25:21 -0800

    Adding codes for welcomepage and added some styles

--- mindful/src/App.js
+++ mindful/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Homepage from './pages/Homepage';
+// import Homepage from './pages/Homepage';
+import Welcome from './pages/welcome/Welcomepage';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Welcome/>
+      
     </div>
   );
 }
