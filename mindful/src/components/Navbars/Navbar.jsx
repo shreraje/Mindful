d61373c347303b373bc1940cf5130a794bbc737a
Rajendra Shrestha <rajendrashr@gmail.com>
2021-01-03 19:44:43 -0800

    Fixing Navbar links

--- mindful/src/components/Navbars/Navbar.jsx
+++ mindful/src/components/Navbars/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+// import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
 
 const Navbar = () => {
     return (
         <nav>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo right">Login</a>
+            <a href="/welcome" className="brand-logo right">Login</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/breathing">Breathing Exercises</Link></li>
