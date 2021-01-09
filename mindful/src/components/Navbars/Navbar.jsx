77a9e821a25e42036c96993f788b09b38a071b58
Rajendra Shrestha <rajendrashr@gmail.com>
2021-01-09 10:00:21 -0800

    Fixing some issues with some hamburger icon and side navbar

--- mindful/src/components/Navbars/Navbar.jsx
+++ mindful/src/components/Navbars/Navbar.jsx
@@ -6,14 +6,13 @@ import text from '../../image/mindful_text.png';
 import 'materialize-css/dist/css/materialize.min.css';
 import './Navbar.css';
 
-window.onload=function(){
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+window.onload = function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
 };
 
 const Navbar = () => {
@@ -28,7 +27,7 @@ const Navbar = () => {
           </div>
           <img src={logo} width="70" height="57" alt="App Logo" className="brand-logo center hoverable"></img>
           <a href="/welcome" className="login right">Login</a>
-          <ul className="nav-links">
+          <ul id="nav-mobile" className="nav-links">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/breathing">Breathing Exercises</Link></li>
             <li><Link to="/video">Video Resources</Link></li>
