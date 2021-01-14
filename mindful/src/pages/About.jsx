0da61141b8ecffc72ecbb358db36d77ebe09246e
Dallin Suppes <suppesd@gmail.com>
2021-01-13 18:19:18 -0700

    Taking background adding minor to about

---
+++ mindful/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../components/Navbars/Navbar';
+import { CardDeck, Card, Col, Row, Jumbotron, Container, Button } from 'react-bootstrap';
+import Footer from '../components/Footer/Footer';
+
+function About() {
+    return(
+        <div>
+            <Navbar/>
+            <Jumbotron fluid>
+
+                    
+            </Jumbotron>
+            <Footer/>
+        </div>
+    );
+};
+
+export default About;
